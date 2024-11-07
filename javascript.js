a7487f165016701d5417b0e1336a8600ed61cdfd
Divine508 <onuorahchidera044@gmail.com>
2024-11-07 16:02:07 +0100

    initial commit

---
+++ javascript.js
@@ -0,0 +1,6 @@
+document
+.getElementById("user-menu-button").addEventListener("click",function (){document.getElementById("menu-dropdown").classList.toggle("hidden")})
+document
+.getElementById("mobile-menu-btn")
+.addEventListener("click", function () 
+{document.getElementById("mb-dropdown-btn").classList.toggle("hidden")})
